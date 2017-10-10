9676818ad3d160692ab75f96ac80dd58fdca8d0d
laurkim <laurakeem@gmail.com>
2017-10-10 17:02:40 -0400

    tests passing, partner is Dylan Kershaw

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,18 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+  get '/' do
+    erb :views
+  end
+
+  post '/teams' do
+    @team = Team.new(params[:team])
+
+    params[:team][:members].each do |member|
+      Hero.new(member)
+    end
+    @heroes = Hero.all
+    erb :team
+  end
 
 end
